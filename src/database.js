96541e3dca0739ff453f59615634b97552bad2d9
Proustibat <jennifer.proust@gmail.com>
2017-12-05 10:39:26 +0100

    chore(Database): change database to split 2 differents db: one for the current app, one for v1

--- src/database.js
+++ src/database.js
@@ -1,20 +1,9 @@
 import Rebase from 're-base';
 import firebase from 'firebase';
-//
-// const database = Rebase.createClass({
-//     apiKey: "AIzaSyBQGGb2Kd_F7XIrqJDZ5LqcdAPv_rWpGWs",
-//     authDomain: "prstbt-udemy-chatbox.firebaseapp.com",
-//     databaseURL: "https://prstbt-udemy-chatbox.firebaseio.com"
-// });
-//
-// export default database;
-
-
-
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyBQGGb2Kd_F7XIrqJDZ5LqcdAPv_rWpGWs",
-    authDomain: "prstbt-udemy-chatbox.firebaseapp.com",
-    databaseURL: "https://prstbt-udemy-chatbox.firebaseio.com",
+    apiKey: "AIzaSyB-0FMLAKI1OVWtuLxlApejit3K5fvJMDA",
+    authDomain: "prstbt-chatbox.firebaseapp.com",
+    databaseURL: "https://prstbt-chatbox.firebaseio.com",
 });
-const database = Rebase.createClass(firebaseApp.database());
+const database = Rebase.createClass( firebaseApp.database() );
 export default database;
